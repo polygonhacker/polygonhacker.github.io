65e1be1a3e277e91db6600cb57b3f53aceade7e2
Mark Park <mark.dm.park@gmail.com>
2022-02-08 14:01:31 -0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Intro from './components/Intro/Intro';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Intro />
+      <About />
+      <Projects />
       <NavBar />
-      <Intro />
-      <Intro />
-      <Intro />
-      <Intro />
     </div>
   );
 }
