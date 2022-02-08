3d6dc08126792bde7fd2fe71dfca88fe3e1dad84
Mark Park <mark.dm.park@gmail.com>
2022-02-08 14:05:19 -0800

    added About comp

---
+++ src/components/About/About.tsx
@@ -0,0 +1,20 @@
+import './About.css'
+
+const About: React.FC = () => {
+    return (
+        <section className='About'>
+            <div className='container'>
+                <h2>About</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sollicitudin fringilla neque, sit amet dapibus tortor dictum et. Integer at vestibulum ex, a rhoncus dui. Sed eleifend quam neque. Suspendisse potenti. Nullam eu ex et nulla mattis feugiat. Cras et ex ut sapien tempor interdum. Phasellus scelerisque, odio vel cursus euismod, dolor lectus ultrices risus, eu ultricies orci felis eu lectus. Donec a aliquam tortor. Fusce placerat et nisi venenatis pharetra. Sed porta erat a nisl efficitur consectetur. Etiam commodo nibh quis pretium malesuada. Donec tincidunt pulvinar molestie. Vivamus at nisl sed lectus tincidunt mollis sit amet vitae mi. Integer sit amet iaculis leo. Phasellus mattis tellus quis pellentesque finibus.
+
+Nam enim lectus, luctus ut imperdiet in, elementum dapibus nulla. Nulla mollis at velit vel viverra. Aenean congue consequat enim, a dapibus ante fringilla sit amet. Etiam hendrerit vestibulum condimentum. Etiam in venenatis dui. Suspendisse eu scelerisque metus, et aliquam quam. Ut quis libero augue. Ut erat augue, aliquam sed lacus sed, euismod porttitor ex.
+
+Nullam dignissim sodales dictum. Nulla id tellus sit amet massa bibendum suscipit. Maecenas lorem lorem, interdum in cursus ac, ultrices vitae orci. Aenean eu tellus vitae lectus lacinia cursus semper vitae turpis. Quisque fringilla, tortor id finibus rhoncus, nisi lectus dictum sapien, vel maximus odio nunc vel metus. In hac habitasse platea dictumst. Aliquam at leo non eros volutpat viverra eget pellentesque enim. Vestibulum et maximus risus. Aenean scelerisque fringilla nunc sed pulvinar. Praesent orci urna, placerat eu laoreet vel, bibendum sit amet sem.
+
+</p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
