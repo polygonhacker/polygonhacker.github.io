a7a56d6f38efa342bc189adf7aa472354801066f
Mark Park <mark.dm.park@gmail.com>
2022-02-21 08:47:54 -0800

    added social media icons and adjusted the design of footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,31 @@
 import './Footer.css';
+import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs'
 
 const Footer: React.FC = () => {
     return (
         <footer className='footer'>
-            <div className='copyright'><h3>© Copyright 2022. Yong Cheol Park</h3></div>
-            <div className='social-media'>
-                <h3>Follow me</h3>
-                <a href='#'></a>
+            <div className='left'>
+                <div>
+                    <h2>Yong Cheol Park</h2>
+                    <h3>polygonhacker [at] gmail [dot] com</h3>
+                </div>
+                <div className='copyright'>
+                    <h4>© Copyright 2022. Yong Cheol Park</h4>
+                </div>
+            </div>
+            
+            
+            <div className='right'>
+                <h2>Follow me</h2>
+                <div className='social-media'>
+                    <a href='#'><BsGithub size={40} /></a>
+                    <a href='#'><BsInstagram size={40} /></a>
+                    <a href='#'><BsLinkedin size={40} /></a>
+                </div>
+                
+                
+
+
             </div>
         </footer>
     )
