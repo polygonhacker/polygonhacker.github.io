4cfce8608022970ded853b0d17adb3fe85014fce
Mark Park <mark.dm.park@gmail.com>
2022-04-27 11:18:13 -0700

    navbar is now responsive to smaller screen

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,17 @@
+import React, {useState} from 'react';
 import './NavBar.css';
+import { ReactComponent as Burger } from '../../assets/menu.svg';
+import { ReactComponent as Download } from '../../assets/download.svg';
+
+
 
 export const NavBar: React.FC = () => {
+    const [burgerOn, setBurgerOn] = useState(false);
+
     return (
         <header className='NavBar'> 
             <div className='name'>Yong Cheol Park</div>
-            <div className='menu-list'>
+            <div className={'menu-list ' + (burgerOn && 'nav-active')}>
                 <ul>
                     <li>
                         <a href='#' data-text='About'>About</a>
@@ -18,11 +25,16 @@ export const NavBar: React.FC = () => {
                     <li>
                         <a href='#' data-text='Contact'>Contact</a>
                     </li>
-                    <li>
-                        <button data-text='Resume'>Resume</button>
+                    <li className='download-button'>
+                        <button data-text=''>
+                            <Download style={{ height: '1.2rem', width: '1.2rem' }} />
+                            <span>&nbsp;Resume</span></button>
                     </li>
                 </ul>
             </div>   
+            <div className='burger' onClick={() => setBurgerOn(!burgerOn)}>
+                <Burger />
+            </div>
         </header>
     )
 }
