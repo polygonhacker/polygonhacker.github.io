93a035b4e1e2fe00512ccaebb69fcc07de40628d
Mark Park <mark.dm.park@gmail.com>
2022-04-30 21:58:27 -0700

    footer switched left and right and is now responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,23 +4,23 @@ import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs'
 const Footer: React.FC = () => {
     return (
         <footer className='footer'>
-            <div className='left'>
-                <div>
-                    <h2>Yong Cheol Park</h2>
-                    <h3>polygonhacker [at] gmail [dot] com</h3>
+            <div className='container'>
+                <div className='left'>
+                    <h2>Follow me</h2>
+                    <div className='social-media'>
+                        <a href='https://github.com/polygonhacker'><BsGithub size={40} /></a>
+                        <a href='https://www.instagram.com/polygonhacker/'><BsInstagram size={40} /></a>
+                        <a href='#'><BsLinkedin size={40} /></a>
+                    </div>
                 </div>
-                <div className='copyright'>
-                    <h4>© Copyright 2022. Yong Cheol Park</h4>
-                </div>
-            </div>
-            
-            
-            <div className='right'>
-                <h2>Follow me</h2>
-                <div className='social-media'>
-                    <a href='#'><BsGithub size={40} /></a>
-                    <a href='#'><BsInstagram size={40} /></a>
-                    <a href='#'><BsLinkedin size={40} /></a>
+                <div className='right'>
+                    <div>
+                        <h2>Yong Cheol Park</h2>
+                        <h3>polygonhacker [at] gmail [dot] com</h3>
+                    </div>
+                    <div className='copyright'>
+                        <h4>© Copyright 2022. Yong Cheol Park</h4>
+                    </div>
                 </div>
             </div>
         </footer>
