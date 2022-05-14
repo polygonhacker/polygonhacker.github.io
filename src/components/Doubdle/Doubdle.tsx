7e0e01e654493a257dd6bc89ebc2bc2cfa6fec51
Mark Park <mark.dm.park@gmail.com>
2022-05-13 17:05:33 -0700

    Doubdle section finished

--- src/components/Doubdle/Doubdle.tsx
+++ src/components/Doubdle/Doubdle.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import './doubdle.css';
+import './Doubdle.css';
 
 const Doubdle = () => {
   return (
     <section className='Doubdle' id='doubdle'>
       <h2>Doubdle</h2>
-      <div className='card-container'>
-        <div className='card'>Doubdle</div>
+      {/* <div className='card-container'>
+        <div className='card'>
+            <div className='top'>
+
+            </div>
+            <div className='bottom'>
+                <h3>Doubdle</h3>
+            </div>
+        </div>
+      </div> */}
+      <div className='description'>
+        <p className='big'>Come play Doubdle!</p>
+        <p className='small'>Guess two 5 letter words in six tries.</p>
+        <a href='https://doubdle.polygonhacker.com' target='_blank' rel='noopener noreferrer'>Play Doubdle</a>
       </div>
       
     </section>
